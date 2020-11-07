f1d189921e9e02d037ab28b6b1d0b7be4646d14d
Navu4 <51900952+Navu4@users.noreply.github.com>
2020-11-08 01:28:07 +0530

    Pattern Question
    Pattern Question from beginner to advance

---
+++ Patterns/half_pyramid.cpp
@@ -0,0 +1,23 @@
+// Print Half Pyramid Pattern using Stars
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int n;
+    cin>>n;
+
+    for(int i = 1 ; i <= n ; i++){
+        for(int j = 1 ; j <= i; j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
